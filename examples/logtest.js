f934d6771f11e49ca15acdc39316601f709b9e3a
James Pravetz <jpravetz@epdoc.com>
2014-05-08 17:45:30 -0700

    Refactored logger middleware so that responseLogger methods are added to the response object rather than being redeclared each time. This should be more efficient. Added examples folder and moved test to logtest.js, then added request to demonstrate how to use stubs.

--- examples/logtest.js
+++ examples/logtest.js
@@ -1,15 +1,7 @@
+#!/usr/bin/env node
 /*************************************************************************
- * ARMOR5 CONFIDENTIAL
- * Copyright 2012 Armor5, Inc. All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains the property
- * of Armor5, Inc. and its suppliers, if any. The intellectual and
- * technical concepts contained herein are proprietary to Armor5, Inc.
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material is
- * strictly forbidden unless prior written permission is obtained from
- * Armor5, Inc..
+ * Copyright(c) 2012-2014 Jim Pravetz <jpravetz@epdoc.com>
+ * May be freely distributed under the MIT license.
  **************************************************************************/
 
 const TIMER_INTERVAL = 2000;
---
+++ examples/reqtest.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+/*************************************************************************
+ * Copyright(c) 2012-2014 Jim Pravetz <jpravetz@epdoc.com>
+ * May be freely distributed under the MIT license.
+ **************************************************************************/
+
+const TIMER_INTERVAL = 2000;
+const BUFFER_INTERVAL = 0; //1000;
+var type = "console";
+//var type = "sos";
+//var type = "file";
+
+var Logger = require('../index');
+Logger.setGlobalLogLevel('verbose');
+var log = require('../index').get('reqtest');
+
+var middleware = Logger.middleware();
+var ResStub = Logger.resStub;
+var res = new ResStub();
+res.onSend = function (msg) {
+    log.info(msg);
+    process.exit(0);
+};
+
+var req = {
+    _startTime: new Date(),
+    send: function (msg) {
+    }
+};
+res.req = req;
+
+middleware.reqId()(req, res, function (err) {
+    if (err) {
+        log.error(error);
+    } else {
+        log.logObj(req);
+        middleware.responseLogger()(req, res, function (err) {
+            if (err) {
+                log.error(error);
+            } else {
+                res.info('Added response logger');
+                middleware.routeSeparator()(req, res, function (err) {
+                    if (err) {
+                        log.error(error);
+                    } else {
+                        res.pushRouteInfo('myrouteinfo');
+                        res.logObj({x: 3, y: 4}).info('Info goes here');
+                        res.success(200, "Finished successfully");
+                    }
+                });
+            }
+        });
+    }
+});
+
+console.log('done');
