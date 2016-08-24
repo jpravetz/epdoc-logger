db227984678693d4c87d8737ed20565938d307e3
Jim Pravetz <jpravetz@gmail.com>
2016-08-24 15:45:19 -0700

    Fixed template console logging when emitter, action are undefined

--- src/format.js
+++ src/format.js
@@ -160,9 +160,9 @@ var self = {
             replace('reqId', params.reqId ? params.reqId : '0');
             replace('sid', params.sid ? params.sid : '');
         }
-        replace('emitter', params.emitter);
-        replace('action', params.action);
-        replace('message', params.message);
+        replace('emitter', params.emitter ? params.emitter : '');
+        replace('action', params.action ? params.action : '' );
+        replace('message', params.message ? params.message : '');
 
         if (options.static) {
             replace('static', JSON.stringify(params.static ? params.static : {}));
